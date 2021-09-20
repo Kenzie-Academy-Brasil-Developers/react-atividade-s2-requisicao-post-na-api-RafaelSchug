89f086fcfeb6c849da5555ab43804d13a8df8a10
Rafael Schug <rafaelschugbc@gmail.com>
2021-09-20 19:02:19 -0300

    add: Display component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import {Login} from './components/Login'
+import {Display} from './components/Display'
 
 function App() {
   return (
     <div className="App">
-      <Login></Login>
+      <Login />
+      <Display />
     </div>
   );
 }
---
+++ src/components/Display/index.jsx
@@ -0,0 +1,7 @@
+import {Container} from './style';
+
+export const Display = () => {
+    return (
+        <Container>Display</Container>
+    )
+}
